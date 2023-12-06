11a81ea8fcbab319756f3ac952a8cabdea0494eb
Dinis Costa <dinispintoc@gmail.com>
2023-12-06 13:45:55 +0000

    Day 6 solutions added.

---
+++ Day06.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023
+{
+    internal class Day06
+    {
+        private readonly string _input;
+        public Day06()
+        {
+            _input = "Day06.txt";
+        }
+
+        public void PartOneAndTwo()
+        {
+            string input = File.ReadAllText(_input);
+            string[] lines = input.Split('\n');
+
+            var times = Regex.Matches(lines[0], @"\d+").Select(match => long.Parse(match.Value));
+            var distances = Regex.Matches(lines[1], @"\d+").Select(match => long.Parse(match.Value));
+
+            var timesDistances = times.Zip(distances, (time, distance) => (time, distance)).ToList();
+
+            var recordList = timesDistances.Select(set =>
+            {
+                return Enumerable.Range(0, (int)(set.time + 1))
+                    .Count(time => time * (set.time - time) > set.distance);
+            }).ToList();
+
+            var mult = recordList.Aggregate((x, y) => x * y);
+            Console.WriteLine(mult);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,17 +1,20 @@
 ﻿using AdventOfCode2023;
 
-Day01 day1 = new();
-day1.PartOne();
-day1.PartTwo();
+//Day01 day1 = new();
+//day1.PartOne();
+//day1.PartTwo();
 
-Day02 day2 = new();
-day2.PartOne();
-day2.PartTwo();
+//Day02 day2 = new();
+//day2.PartOne();
+//day2.PartTwo();
 
-Day03 day3 = new();
-day3.PartOne();
-day3.PartTwo();
+//Day03 day3 = new();
+//day3.PartOne();
+//day3.PartTwo();
 
-Day04 day4 = new();
-day4.PartOne();
-day4.PartTwo();
+//Day04 day4 = new();
+//day4.PartOne();
+//day4.PartTwo();
+
+Day06 day6 = new();
+day6.PartOneAndTwo();
